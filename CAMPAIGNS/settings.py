5bb64ce4d6e35dc5b87b047cd3129f04e4197e69
Deepak Rajpurohit <princerajpurohit945@gmail.com>
2022-12-18 21:51:27 +0530

    cloud chnage

--- CAMPAIGNS/settings.py
+++ CAMPAIGNS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", 'django-insecure-chm2=7o$x7s8l7846w-8n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", 'deepak963635.pythonanywhere.com']
 
 
 # Application definition
